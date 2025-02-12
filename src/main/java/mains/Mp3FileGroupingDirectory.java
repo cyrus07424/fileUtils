cab6428f170381858380657d952046a838f8eb7c
cyrus <j07424-development@yahoo.co.jp>
2025-02-12 18:38:01 +0900

    update: 処理を最新化

--- src/main/java/mains/Mp3FileGroupingDirectory.java
+++ src/main/java/mains/Mp3FileGroupingDirectory.java
@@ -1,6 +1,8 @@
 package mains;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -8,6 +10,7 @@ import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.TagField;
 
 /**
  * MP3ファイル一覧のアルパム名からディレクトリを統合.
@@ -20,7 +23,23 @@ public class Mp3FileGroupingDirectory {
 	 * 対象のディレクトリ.<br>
 	 * このディレクトリ直下のディレクトリに対して処理を実行する.
 	 */
-	private static File TARGET_DIRECTROY = new File("CHANGE ME");
+	private static final File TARGET_DIRECTROY = new File("CHANGE ME");
+
+	/**
+	 * 使用するフィールドの一覧(優先度が高い順).
+	 */
+	private static final FieldKey[] FIELDKEY_ARRAY = new FieldKey[] { FieldKey.ALBUM, FieldKey.ALBUM_ARTIST,
+			FieldKey.ALBUM_ARTISTS, FieldKey.ARTIST, FieldKey.ARTISTS };
+
+	/**
+	 * デバッグモード(ディレクトリ移動を行わない).
+	 */
+	private static final boolean DEBUG_MODE = true;
+
+	/**
+	 * FIXME 使用する文字セット.
+	 */
+	private static final String USE_CHARSET = "SHIFT_JIS";
 
 	/**
 	 * main.
@@ -38,67 +57,49 @@ public class Mp3FileGroupingDirectory {
 						// オーディオファイルとして読み込み
 						AudioFile audioFile = AudioFileIO.read(file);
 
-						try {
-							// タグを取得
-							Tag tag = audioFile.getTag();
+						// タグを取得
+						Tag tag = audioFile.getTag();
+						if (DEBUG_MODE) {
+							// 全てのフィールドに対して実行
+							Iterator<TagField> tagFieldIterator = tag.getFields();
+							while (tagFieldIterator.hasNext()) {
+								// FIXME フィールドの値を取得
+								TagField tagField = tagFieldIterator.next();
+								System.out.println(
+										tagField.getId() + ": " + new String(tagField.getRawContent()));
+								System.out.println(
+										tagField.getId() + ": "
+												+ new String(tagField.getRawContent(), StandardCharsets.ISO_8859_1));
+								System.out.println(
+										tagField.getId() + ": " + new String(tagField.getRawContent(), "EUC-JP"));
+								System.out.println(
+										tagField.getId() + ": " + new String(tagField.getRawContent(), USE_CHARSET));
+							}
+						}
 
-							// タグが存在する場合
-							if (tag != null) {
-								try {
-									// アルバム名を取得
-									String album = StringUtils.strip(tag.getFirst(FieldKey.ALBUM));
-									System.out.println("album: " + album);
+						// タグが存在する場合
+						if (tag != null) {
+							// 全ての使用するフィールドに対して実行
+							for (FieldKey fieldKey : FIELDKEY_ARRAY) {
+								// フィールドの値を取得
+								String fieldValue = getFieldFromTag(tag, fieldKey);
+								System.out.println(fieldKey.name() + ": " + fieldValue);
 
-									// アルバム名が存在する場合
-									if (StringUtils.isNotBlank(album)) {
+								// フィールドの値が存在する場合
+								if (StringUtils.isNotBlank(fieldValue)) {
+									if (!DEBUG_MODE) {
 										// 子ディレクトリを作成
-										File childDirectory = new File(TARGET_DIRECTROY, sanitizeDirectoryName(album));
+										File childDirectory = new File(TARGET_DIRECTROY,
+												sanitizeDirectoryName(fieldValue));
 
 										// ファイルを子ディレクトリへ移動
 										FileUtils.moveFileToDirectory(file, childDirectory, true);
 									}
-								} catch (Exception e1) {
-									e1.printStackTrace();
-									try {
-										// アルバムのアーティストを取得
-										String albumArtist = StringUtils
-												.strip(audioFile.getTag().getFirst(FieldKey.ALBUM_ARTIST));
-										System.out.println("albumArtist: " + albumArtist);
 
-										// アルバムのアーティストが存在する場合
-										if (StringUtils.isNotBlank(albumArtist)) {
-											// 子ディレクトリを作成
-											File childDirectory = new File(TARGET_DIRECTROY,
-													sanitizeDirectoryName(albumArtist));
-
-											// ファイルを子ディレクトリへ移動
-											FileUtils.moveFileToDirectory(file, childDirectory, true);
-										}
-									} catch (Exception e2) {
-										e2.printStackTrace();
-										try {
-											// アルバムのアーティストを取得
-											String albumArtists = StringUtils
-													.strip(audioFile.getTag().getFirst(FieldKey.ALBUM_ARTISTS));
-											System.out.println("albumArtists: " + albumArtists);
-
-											// アルバムのアーティストが存在する場合
-											if (StringUtils.isNotBlank(albumArtists)) {
-												// 子ディレクトリを作成
-												File childDirectory = new File(TARGET_DIRECTROY,
-														sanitizeDirectoryName(albumArtists));
-
-												// ファイルを子ディレクトリへ移動
-												FileUtils.moveFileToDirectory(file, childDirectory, true);
-											}
-										} catch (Exception e3) {
-											e3.printStackTrace();
-										}
-									}
+									// 終了
+									break;
 								}
 							}
-						} catch (Exception e3) {
-							e3.printStackTrace();
 						}
 					}
 				} catch (Exception e) {
@@ -113,7 +114,24 @@ public class Mp3FileGroupingDirectory {
 	}
 
 	/**
+	 * タグからフィールドの値を取得.
+	 * 
+	 * @param tag
+	 * @param fieldKey
+	 * @return
+	 */
+	private static String getFieldFromTag(Tag tag, FieldKey fieldKey) {
+		try {
+			return StringUtils.strip(tag.getFirst(fieldKey));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
 	 * ディレクトリ名をサニタイズ.
+	 * 
 	 * @param directoryName
 	 * @return
 	 */
